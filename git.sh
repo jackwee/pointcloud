16c29bf95b3491d62bbdeff5767fd1d38c52a1fd
jackwee <zjw_xd@163.com>
2013-05-13 14:43:29 +0800

    upload file...

--- git.sh
+++ git.sh
@@ -1,7 +1,11 @@
 
 echo +++++++++++++++++++++
-cd pointcloud 
+cd c:
+pwd
+cd  Users/Administrator/pointcloud
+pwd
 git add .
 git commit -m "upload file..."
 git push origin gh-pages
+echo --------------------------
 
